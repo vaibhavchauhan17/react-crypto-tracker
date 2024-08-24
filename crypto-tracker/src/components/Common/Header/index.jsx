6dd434c5997a803ea412ae19d13619a342da616a
Vaibhav Kumar Chauhan <vaibhavchauhan17>
2024-08-24 22:55:30 +0530

    Header added

---
+++ crypto-tracker/src/components/Common/Header/index.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./styles.css";
+import TemporaryDrawer from "./drawer";
+import Button from "../Button";
+
+const Header = () => {
+  return (
+    <div className="navbar">
+      <h1 className="logo">
+        CryptoTracker<span style={{ color: "var(--blue" }}>.</span>
+      </h1>
+      <div className="links">
+        <a href="/">
+          <p className="link">Home</p>
+        </a>
+        <a href="/">
+          <p className="link">Compare</p>
+        </a>
+        <a href="/">
+          <p className="link">Watchlist</p>
+        </a>
+        <a href="#">
+          <Button
+            text={"Dashboard"}
+            onClick={() => console.log("BTN-clicked")}
+          />
+        </a>
+      </div>
+      <div className="drawer-component">
+        <TemporaryDrawer />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
