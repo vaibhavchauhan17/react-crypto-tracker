bf2f87e6c7a3e8c1eaaf73ff4d576ac15f4f55d2
Vaibhav Kumar Chauhan <vaibhavchauhan17>
2024-10-23 20:08:50 +0530

    almost done commit

--- crypto-tracker/src/components/Coin/LineChart/index.jsx
+++ crypto-tracker/src/components/Coin/LineChart/index.jsx
@@ -17,10 +17,30 @@ function LineChart({ chartData, multiAxis, priceType }) {
     },
     scales: {
       crypto1: {
+        type: "linear",
+        display: true,
         position: "left",
+        ticks: {
+          callback: function (value, index, ticks) {
+            if (priceType == "prices") return "$" + value.toLocaleString();
+            else {
+              return "$" + convertNumber(value);
+            }
+          },
+        },
       },
       crypto2: multiAxis && {
+        type: "linear",
+        display: true,
         position: "right",
+        ticks: {
+          callback: function (value, index, ticks) {
+            if (priceType == "prices") return "$" + value.toLocaleString();
+            else {
+              return "$" + convertNumber(value);
+            }
+          },
+        },
       },
       //   y: {
       //     ticks: {
--- crypto-tracker/src/pages/DashBoard.jsx
+++ crypto-tracker/src/pages/DashBoard.jsx
@@ -6,13 +6,14 @@ import Search from "../components/Dashboard/Search";
 import PaginationComponent from "../components/Dashboard/Pagination";
 import Loader from "../components/Common/Loader";
 import BackToTop from "../components/Common/BackToTop";
+import { get100Coins } from "../functions/get100Coins";
 
 const DashBoard = () => {
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   const [paginatedCoins, setPaginatedCoins] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const handleChange = (e) => {
     setSearch(e.target.value);
@@ -24,30 +25,26 @@ const DashBoard = () => {
     setPaginatedCoins(coins.slice(previousIndex, previousIndex + 10));
   };
 
-  var filteredCoins = coins.filter(
+  useEffect(() => {
+    //fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const myCoins = await get100Coins();
+    if (myCoins) {
+      setCoins(myCoins);
+      setPaginatedCoins(myCoins?.slice(0, 10));
+      setLoading(false);
+    }
+  };
+
+  var filteredCoins = coins?.filter(
     (item) =>
       item.name.toLowerCase().includes(search.toLowerCase()) ||
       item.symbol.toLowerCase().includes(search.toLowerCase())
   );
 
-  useEffect(() => {
-    setLoading(true);
-    //fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
-    axios
-      .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-      )
-      .then((response) => {
-        console.log("Response>>>", response);
-        setCoins(response.data);
-        setPaginatedCoins(response.data.slice(0, 10));
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log("Error>>>", error);
-        setLoading(false);
-      });
-  }, []);
   return (
     <>
       <Header />
